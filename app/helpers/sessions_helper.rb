0065fa6acd6bc7c56df868dfc86020e1430f0270
pokimadi <pokimadim@hotmail.com>
2012-09-16 09:37:18 -0400

    view all users, user edit url, stooped before faker

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,7 @@ module SessionsHelper
     @current_user = user
   end
 
-  def current_user # close mozilla leaves it sstill active 
+  def current_user # close mozilla leaves it still active 
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
   
@@ -22,6 +22,20 @@ module SessionsHelper
     current_user = nil
     cookies.delete(:remember_token)
   end
+
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+  
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
 
 
 
